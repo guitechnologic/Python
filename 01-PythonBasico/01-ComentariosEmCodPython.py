4a134db279bbbddebf90e16bf67b0cab33e62832
guitechnologic <39844853+guitechnologic@users.noreply.github.com>
2021-11-29 17:08:22 -0300

    first commit

---
+++ 01-PythonBasico/01-ComentariosEmCodPython.py
@@ -0,0 +1,8 @@
+print('Linha    1')
+print('Linha    2')
+#   Comentarios em Python começan com Cerquilha #
+print('Linha    3')
+print('Linha    4')     
+'comentario'
+print('Linha    5')
+print('Linha    6')     #   Comentarios em Python começan com Cerquilha #
---
+++ 01-PythonBasico/02-ComandoPrint.py
@@ -0,0 +1,25 @@
+#print(123456)
+#print('Guilherme',  'Goulart')
+
+#   sep='-' é o separador do print
+#   end=''  é o caracter que será adicionado ao final do print
+print('Guilherme','Goulart', sep='-', end=' ')
+
+
+
+
+
+print('824','176','070',sep='.', end='-')
+print('18')
+
+
+
+
+
+
+
+
+
+
+
+
