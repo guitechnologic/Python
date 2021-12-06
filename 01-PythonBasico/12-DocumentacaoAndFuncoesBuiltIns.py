34982b34ccc6b29ae2cedd37f45dab3b8256737c
guitechnologic <39844853+guitechnologic@users.noreply.github.com>
2021-12-06 16:34:15 -0300

    Documentacao e pass and elipsis

--- 01-PythonBasico/12-DocumentacaoAndFuncoesBuiltIns.py
+++ 01-PythonBasico/12-DocumentacaoAndFuncoesBuiltIns.py
@@ -6,10 +6,10 @@ num2 = input('Digite outra coisa:   ')
 num1 = int(num1)
 num2 = int(num2)
 
-if is_number(num1) and is_number(num2):
+try:
     num1 = float(num1)
     num2 = float(num2)
 
     print(num1 + num2)
-else:
+except:
     print('erro')
---
+++ 01-PythonBasico/13-PassAndElipsisComPlaceHolders.py
@@ -0,0 +1,14 @@
+valor = True
+
+if valor:
+    print('OI')
+else:
+    print('Tchau')
+
+
+valor = false
+
+if valor:
+    pass
+else:
+    print('Tchau')
